b71c70a4e616d50d5bf55af579c92c6fc84c89f1
Thomas Hudson <snarfblam@gmail.com>
2018-03-12 19:48:31 -0400

    displayTables() and makeReservation()

--- front.js
+++ front.js
@@ -5,16 +5,55 @@ var reservationApiUrl = "api/tables";
 var tablesApiUrl = "api/new";
 
 var ui = {
-    tableList: $(".tableList"),
-    waitList: $(".waitList"),
-    inputName: $(".reservationName"),
-    inputPhone: $(".reservationPhone"),
-    inputEmail: $(".reservationEmail"),
-    inputId: $(".reservationID"),
+    tableList: $("#currentRes"),
+    waitList: $("#waitList"),
+    inputName: $("#reservationName"),
+    inputPhone: $("#reservationPhone"),
+    inputEmail: $("#reservationEmail"),
+    inputId: $("#reservationID"),
 }
 
+if (ui.tableList.length > 0) {
+    displayTables();
+}
 
 function displayTables() {
+    $.ajax({
+        url: tablesApiUrl,
+        method: "GET",
+    }).then(function (result) {
+        alert(result);
+        var index = 0;
+        var isWaitList = 0;
+
+        for (var i = 0; i < result.length; i++) {
+            var itemLi = createTableListItem(index, result[i].id);
+            index++;
+
+            if (isWaitList) {
+                ui.waitList.append(itemLi);
+            } else {
+                ui.tableList.append(itemLi);
+                if (index == 5) {
+                    index = 0;
+                    isWaitList = true;
+                }
+            }
+        }
+    }).catch(function (error) {
+        alert(error);
+        alert("There has been an error submitting your reservation.");
+    })
+}
+
+function createTableListItem(text, index) {
+    var item = $("<li>").text(" - " + text);
+    item.prepend($('<span>').addClass('label label-primary').text(index));
+
+    return item;
+}
+
+function makeReservation() {
     var reservationData = {
         name: ui.inputName.text(),
         phone: ui.inputPhone.text(),
@@ -24,7 +63,8 @@ function displayTables() {
 
     $.ajax({
         url: reservationApiUrl,
-        method: "GET",
+        method: "POST",
+        data: reservationData,
     }).then(function (result) {
         alert(result);
         alert("Your reservation has been made");
